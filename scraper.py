5c26d30c2d46faad4efa889a2fb7d6524f0a710b
Chen Bo Calvin Zhang <chen.zhang-10@student.manchester.ac.uk>
2020-03-25 16:25:57 +0000

    Scrape Reddit page for images

--- scraper.py
+++ scraper.py
@@ -1,9 +1,32 @@
+import io
+from PIL import Image
+import os
+import hashlib
 import requests
 import csv
 import time
 from bs4 import BeautifulSoup as soup
 
-url = "https://old.reddit.com/r/datascience/"
+def persist_image(folder_path:str, url:str):
+  try:
+    image_content = requests.get(url).content
+
+  except Exception as e:
+    print(f"ERROR - Could not download {url} - {e}")
+
+  try:
+    image_file = io.BytesIO(image_content)
+    image = Image.open(image_file).convert('RGB')
+    file_path = os.path.join(folder_path, hashlib.sha1(image_content).hexdigest()[:10] + '.jpg')
+    with open(file_path, 'wb') as f:
+      image.save(f, "JPEG", quality=85)
+    print(f"SUCCESS - saved {url} - as {file_path}")
+  except Exception as e:
+    print(f"ERROR - Could not save {url} - {e}")
+
+
+
+url = "https://old.reddit.com/r/Coronavirus_Meme/"
 # Headers to mimic a browser visit
 headers = {'User-Agent': 'Mozilla/5.0'}
 
@@ -12,33 +35,27 @@ page = requests.get(url, headers=headers)
 
 soup_page = soup(page.text, 'html.parser')
 
-attrs = {'class': 'thing', 'data-domain': 'self.datascience'}
-posts = soup_page.find_all('div', attrs=attrs)
+attrs = {'class': 'thing', 'data-domain': 'i.redd.it'}
 
 counter = 1
 
-while (counter <= 100):
-    for post in posts:
-        # print(post.attrs['data-domain'])
-        title = post.find('p', class_="title").text
-        author = post.find('a', class_='author').text
-        comments = post.find('a', class_='comments').text
-        if comments == "comment":
-            comments = 0
+while (counter <= 30):
+  posts = soup_page.find_all('div', attrs=attrs)
+  for post in posts:
+    image = post.find("a", class_="thumbnail")
+    image_page_link = 'http://old.reddit.com' + image.attrs['href'] + '?'
+    image_page = requests.get(image_page_link, headers=headers)
+    image_soup_page = soup(image_page.text, 'html.parser')
+    
+    file = image_soup_page.find("img", class_="preview")
+    file_link = file.attrs['src']
+    
+    persist_image('./images', file_link)
 
-        likes = post.find("div", attrs={"class": "score likes"}).text
-        if likes == "•":
-            likes = "None"
+    counter += 1
 
-        post_line = [counter, title, author, likes, comments]
-        with open('output.csv', 'a') as f:
-            writer = csv.writer(f)
-            writer.writerow(post_line)
-
-        counter += 1
-
-    next_button = soup_page.find("span", class_="next-button")
-    next_page_link = next_button.find("a").attrs['href']
-    time.sleep(2)
-    page = requests.get(next_page_link, headers=headers)
-    soup_page = soup(page.text, 'html.parser')
+  next_button = soup_page.find("span", class_="next-button")
+  next_page_link = next_button.find("a").attrs['href']
+  time.sleep(2)
+  page = requests.get(next_page_link, headers=headers)
+  soup_page = soup(page.text, 'html.parser')
